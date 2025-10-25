e56244d2ecd0cae5ad05f004753016ec7573e478
Ashvatth Joshi <codingash27@gmail.com>
2025-10-25 14:26:18 +0530

    notepad monaco y d

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import Editor from "@monaco-editor/react";
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { MonacoBinding } from "y-monaco";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0);
+  const editorRef = useRef<any>(null);
+
+  function handleEditorMount(editor, monaco) {
+    editorRef.current = editor;
+    // Initialize y
+    const doc = new Y.Doc();
+
+    // connect to webrtc
+    const provider = new WebrtcProvider("test-room", doc);
+    const type = doc.getText("monaco");
+
+    // bind to Monaco
+    const binding = new MonacoBinding(
+      type,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    );
+
+    console.log(provider.awareness);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Editor
+      height="100vh"
+      width="100vw"
+      theme="vs-light"
+      onMount={handleEditorMount}
+    />
+  );
 }
 
-export default App
+export default App;
